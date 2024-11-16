e2362ce159b8c4d9d58f391f4b6ef2b50c9632e6
CHIBUIKEM DIDIGU KENECHUKWU <116978933+chibzdee@users.noreply.github.com>
2024-11-15 20:44:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+rom flask import Flask, render_template, request, send_from_directory
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, FileField
 from werkzeug.utils import secure_filename
